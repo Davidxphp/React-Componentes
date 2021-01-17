f9baeb36f732dcba142ee691efa71f5282c5ea06
davidxphp <davidxphp@gmail.com>
2021-01-17 16:12:21 -0300

    first commit

---
+++ src/componentes/Card/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css';
+
+export default function Card() {
+    return (
+        <div className="card-container">
+            <p>componentes</p>
+            <p>facilita em manter partes memores funcionando corretamente</p>
+            <p>Você pode reutilizá-los, ou seja, memos para se escrever</p>
+        </div>
+    );
+}
